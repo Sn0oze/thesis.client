5baf3734572f7c91e29d68d0b9ef1fb98b99b46c
Jan <janewaldable@gmail.com>
2020-02-22 15:17:52 +0100

    moved calendar component to shared module.

--- insights/src/app/shared/shared.module.ts
+++ insights/src/app/shared/shared.module.ts
@@ -2,13 +2,19 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { KeyboardAnnotationComponent } from './keyboard-annotation/keyboard-annotation.component';
 import { StylusAnnotationComponent } from './stylus-annotation/stylus-annotation.component';
+import {CalendarComponent} from './visualisations/calendar/calendar.component';
 
 
 
 @NgModule({
-  declarations: [KeyboardAnnotationComponent, StylusAnnotationComponent],
+  declarations: [
+    KeyboardAnnotationComponent,
+    StylusAnnotationComponent,
+    CalendarComponent],
   exports: [
-    KeyboardAnnotationComponent
+    KeyboardAnnotationComponent,
+    StylusAnnotationComponent,
+    CalendarComponent
   ],
   imports: [
     CommonModule
--- insights/src/app/visualisations/visualisations.module.ts
+++ insights/src/app/visualisations/visualisations.module.ts
@@ -1,14 +1,11 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { CalendarComponent } from './calendar/calendar.component';
 
 
 
 @NgModule({
-    declarations: [CalendarComponent],
-    exports: [
-        CalendarComponent
-    ],
+    declarations: [],
+    exports: [],
     imports: [
         CommonModule
     ]

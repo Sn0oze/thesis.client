b8612e32e4560b2776c82a5fc0e675d091f33e49
Jan <janewaldable@gmail.com>
2020-03-07 20:56:34 +0100

    added pen size selection. added fade out animations to whole toolbar.

--- insights/src/app/shared/components/visualisations/draw-canvas/canvas-toolbar/canvas-toolbar.component.ts
+++ insights/src/app/shared/components/visualisations/draw-canvas/canvas-toolbar/canvas-toolbar.component.ts
@@ -1,22 +1,29 @@
 import {Component, EventEmitter, OnInit, Output} from '@angular/core';
-import {fadeInStagger} from '../../../../animations';
+import {fadeInOut, fadeInStagger} from '../../../../animations';
 
 @Component({
   selector: 'app-canvas-toolbar',
   templateUrl: './canvas-toolbar.component.html',
   styleUrls: ['./canvas-toolbar.component.scss'],
-  animations: [fadeInStagger]
+  animations: [fadeInStagger, fadeInOut]
 })
 export class CanvasToolbarComponent implements OnInit {
   @Output() colorPicked = new EventEmitter<string>();
+  @Output() widthPicked = new EventEmitter<string>();
   colors = ['#4c81c1', '#c58145', '#95ba8a'];
+  width: string;
 
   constructor() { }
 
   ngOnInit(): void {
+    this.width = '2px';
   }
 
   changeColor(color: string): void {
    this.colorPicked.emit(color);
+  }
+
+  changeWidth(): void {
+    this.widthPicked.emit(this.width);
   }
 }

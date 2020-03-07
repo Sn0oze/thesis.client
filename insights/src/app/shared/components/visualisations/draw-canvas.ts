8b0bbd740772f128d01613d474ccd01164c8f60f
Jan <janewaldable@gmail.com>
2020-03-07 22:04:32 +0100

    changed default color and width to first value of constants.

--- insights/src/app/shared/components/visualisations/draw-canvas.ts
+++ insights/src/app/shared/components/visualisations/draw-canvas.ts
@@ -1,5 +1,6 @@
 import * as d3 from 'd3';
 import * as simplify from 'simplify-js';
+import {COLORS, PEN_WIDTHS} from '../../constants';
 
 export class DrawCanvas {
   private svg: any;
@@ -14,8 +15,8 @@ export class DrawCanvas {
 
   constructor(
     private container: HTMLElement,
-    public color = 'rgba(50,130,250, .6)',
-    public strokeWidth = '2px') {
+    public color = COLORS[0], // 'rgba(50,130,250, .6)'
+    public strokeWidth = PEN_WIDTHS[0]) {
     this.init();
   }
 

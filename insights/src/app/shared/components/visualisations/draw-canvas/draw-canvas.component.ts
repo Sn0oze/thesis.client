f656b11806dacbb89eeb429a9869a8b044daa4fe
Jan <janewaldable@gmail.com>
2020-03-07 21:25:47 +0100

    removed default values. added with update on change.

--- insights/src/app/shared/components/visualisations/draw-canvas/draw-canvas.component.ts
+++ insights/src/app/shared/components/visualisations/draw-canvas/draw-canvas.component.ts
@@ -1,4 +1,4 @@
-import {AfterViewInit, Component, ElementRef, NgZone, OnInit, ViewChild} from '@angular/core';
+import {AfterViewInit, Component, ElementRef, Input, NgZone, OnChanges, OnInit, SimpleChanges, ViewChild} from '@angular/core';
 import {DrawCanvas} from '../draw-canvas';
 
 @Component({
@@ -6,8 +6,10 @@ import {DrawCanvas} from '../draw-canvas';
   templateUrl: './draw-canvas.component.html',
   styleUrls: ['./draw-canvas.component.scss']
 })
-export class DrawCanvasComponent implements OnInit, AfterViewInit {
+export class DrawCanvasComponent implements OnInit, AfterViewInit, OnChanges {
   @ViewChild('canvasContainer') canvasContainerRef: ElementRef;
+  @Input() color: string;
+  @Input() width: string;
   canvasContainer: HTMLElement;
   canvas: DrawCanvas;
   constructor( private zone: NgZone) { }
@@ -19,5 +21,16 @@ export class DrawCanvasComponent implements OnInit, AfterViewInit {
       this.canvasContainer = this.canvasContainerRef.nativeElement;
       this.canvas = new DrawCanvas(this.canvasContainer);
     });
+  }
+
+  ngOnChanges(changes: SimpleChanges): void {
+    const color = changes?.color?.currentValue;
+    const width = changes?.width?.currentValue;
+    if (this.canvas && color) {
+      this.canvas.color = color;
+    }
+    if (this.canvas && width) {
+      this.canvas.strokeWidth = width;
+    }
   }
 }

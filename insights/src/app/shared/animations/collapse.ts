753d1e13fde0a8c73d5876d8d469ae07811be27c
Jan <janewaldable@gmail.com>
2020-03-08 16:07:19 +0100

    added alternative collapse fo ngif.

--- insights/src/app/shared/animations/collapse.ts
+++ insights/src/app/shared/animations/collapse.ts
@@ -1,4 +1,4 @@
-import {animate, state, style, transition, trigger} from '@angular/animations';
+import {animate, group, query, state, style, transition, trigger} from '@angular/animations';
 export  const collapseTiming = '225ms cubic-bezier(0.4,0.0,0.2,1)';
 
 export const collapseHorizontal = trigger('collapseHorizontal', [
@@ -9,5 +9,19 @@ export const collapseHorizontal = trigger('collapseHorizontal', [
     width : '*', visibility: 'visible', opacity: 1
   })),
   transition('true <=> false', animate(collapseTiming)),
+]);
+
+export const collapseLeaveEnter = trigger('collapseLeaveEnter', [
+  transition('* => *', [ // each time the binding value changes
+    query(':leave', [
+      animate(collapseTiming, style({ width : '0', opacity: 0 }))
+    ],  { optional: true }),
+    query(':enter', [
+      group([
+        style({ opacity: 0 }),
+        animate(collapseTiming, style({ width : '*', opacity: 1 })),
+      ])
+    ],  { optional: true })
+  ])
 ]);
 

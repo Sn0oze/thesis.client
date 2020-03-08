ffc4dab3e3f884aac2f3c232663caa0b88f296e2
Jan <janewaldable@gmail.com>
2020-03-08 13:03:41 +0100

    changed change detection strategy. changes transform duration.

--- insights/src/app/shared/components/color-patch/color-patch.component.ts
+++ insights/src/app/shared/components/color-patch/color-patch.component.ts
@@ -1,9 +1,10 @@
-import {Component, Input, OnInit} from '@angular/core';
+import {ChangeDetectionStrategy, Component, Input, OnInit} from '@angular/core';
 
 @Component({
   selector: 'app-color-patch',
   templateUrl: './color-patch.component.html',
-  styleUrls: ['./color-patch.component.scss']
+  styleUrls: ['./color-patch.component.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class ColorPatchComponent implements OnInit {
   @Input() color: string;

56c467f689f06ec046e588020bf9252a0aa6ec40
Jan <janewaldable@gmail.com>
2020-03-15 16:10:58 +0100

    refactored dataset model.

--- insights/src/app/shared/models/DataSet.model.ts
+++
@@ -1,8 +0,0 @@
-import * as moment from 'moment';
-
-export interface DataSet {
-  min: moment.Moment;
-  max: moment.Moment;
-  duration: number;
-  data: any;
-}
---
+++ insights/src/app/shared/models/data-set.model.ts
@@ -0,0 +1,21 @@
+import * as moment from 'moment';
+import {Observation} from './observation.model';
+
+export interface DataSet {
+  min: moment.Moment;
+  max: moment.Moment;
+  duration: number;
+  days: Array<DayNest>;
+}
+
+export interface DayNest {
+  key: string;
+  values: Array<HourNest>;
+  value?: any;
+  total?: any;
+}
+
+export interface HourNest {
+  key: string;
+  values: Array<Observation>;
+}

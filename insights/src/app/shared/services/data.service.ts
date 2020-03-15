c102653548d185f5e0569234481fa5d3e2fdbbab
Jan <janewaldable@gmail.com>
2020-03-15 16:12:50 +0100

    added total of observations per day to outer day nest.

--- insights/src/app/shared/services/data.service.ts
+++ insights/src/app/shared/services/data.service.ts
@@ -2,7 +2,7 @@ import { Injectable } from '@angular/core';
 import * as d3 from 'd3';
 import {Observable} from 'rxjs';
 import * as moment from 'moment';
-import {DataSet, Observation} from '../models';
+import {DataSet, DayNest, Observation} from '../models';
 
 @Injectable({
   providedIn: 'root'
@@ -11,28 +11,37 @@ export class DataService {
 
   constructor() { }
 
+  rowConverter(row): Observation {
+    return {
+      timeStamp: row[0],
+      date: moment(row[0]).utcOffset(row[1]),
+      offset: row[1]
+    };
+  }
+
   loadCSV(): Observable<DataSet> {
     return new Observable(observer => {
       d3.text('assets/data/ptsd_filtered.csv').then(text => {
-        const data = d3.csvParseRows(text).map(row => {
-          return {
-            timeStamp: row[0],
-            date: moment(row[0]).utcOffset(row[1]),
-            offset: row[1]
-          } as Observation;
-        });
+        const data = d3.csvParseRows(text).map(this.rowConverter);
         const min = d3.min(data.map(d => d.date));
         const max = d3.max(data.map(d => d.date));
+
         const nested = d3.nest()
           .key((row: Observation) => row.date.format('DD-MM-YYYY'))
-          .key((g: Observation) => g.date.format('HH'))
-          .entries(data);
+          .key((days: Observation) => days.date.format('HH'))
+          .entries(data) as Array<DayNest>;
+
+        nested.map((d) => {
+          d.total = d.values.reduce((total, hour) => total + hour.values.length, 0);
+        });
+
         const dataSet = {
           min,
           max,
           duration: max.diff(min, 'days') + 1,
-          data: nested
-        } as DataSet;
+          days: nested
+        };
+
         observer.next(dataSet);
         observer.complete();
       }, error => observer.next(error));

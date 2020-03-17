bba06a7a42c029fdf5859e0595e00a581245de2a
Jan <janewaldable@gmail.com>
2020-03-17 14:25:20 +0100

    added date and time format.

--- insights/src/app/shared/utils/extended-moment.ts
+++ insights/src/app/shared/utils/extended-moment.ts
@@ -2,3 +2,6 @@ import * as m from 'moment';
 import {extendMoment} from 'moment-range';
 
 export const moment = extendMoment(m);
+
+export const dateFormat = 'DD-MM-YYYY';
+export const hourFormat = 'HH';

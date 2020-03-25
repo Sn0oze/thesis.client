971d7eb8ff35a1d53a87443067cd5fcdbbc0c546
Jan <janewaldable@gmail.com>
2020-03-25 17:41:36 +0100

    added calendar specific constants.

---
+++ insights/src/app/shared/constants/calendar.ts
@@ -0,0 +1,3 @@
+export const CELL_WIDTH = 32;
+export const SPACING_LEFT = 42;
+export const SPACING_RIGHT = 12;
--- insights/src/app/shared/constants/index.ts
+++ insights/src/app/shared/constants/index.ts
@@ -1,2 +1,3 @@
 export * from './colors';
 export * from './pen-widths';
+export * from './calendar';

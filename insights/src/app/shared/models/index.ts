e21799c525963b1cf5401e08794346a51497fcde
Jan <janewaldable@gmail.com>
2020-04-29 14:33:48 +0200

    deleted graph datum.

--- insights/src/app/shared/models/index.ts
+++ insights/src/app/shared/models/index.ts
@@ -1,6 +1,5 @@
 export * from './calendar.model';
 export * from './observation.model';
 export * from './data-set.model';
-export * from './graph-datum';
 export * from './types';
 export * from './category.model';

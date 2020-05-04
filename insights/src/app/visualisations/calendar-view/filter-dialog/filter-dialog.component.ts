be40e5d4af0bc496fc3d68d8abd79447c2f6a7df
Jan <janewaldable@gmail.com>
2020-05-04 17:26:10 +0200

    removed unused functions and imports

--- insights/src/app/visualisations/calendar-view/filter-dialog/filter-dialog.component.ts
+++ insights/src/app/visualisations/calendar-view/filter-dialog/filter-dialog.component.ts
@@ -1,6 +1,6 @@
 import {Component, Inject, OnInit} from '@angular/core';
-import {MAT_DIALOG_DATA, MatDialogRef} from '@angular/material/dialog';
-import {ObservationGroup, ObservationsMap} from '../../../shared/models';
+import {MAT_DIALOG_DATA} from '@angular/material/dialog';
+import {ObservationsMap} from '../../../shared/models';
 import {dateFormat, moment} from '../../../shared/utils';
 
 @Component({
@@ -13,19 +13,10 @@ export class FilterDialogComponent implements OnInit {
   hours = ['01', '02', '03', '04', '05', '06', '07', '08', '09', '10', '11', '12', '13', '14', '15', '16', '17', '18', '19', '20', '21',
     '22', '23', '00'
   ];
-  constructor(public dialogRef: MatDialogRef<any>,
-              @Inject(MAT_DIALOG_DATA) public data: ObservationsMap) { }
+  constructor(@Inject(MAT_DIALOG_DATA) public data: ObservationsMap) { }
 
   ngOnInit(): void {
     this.days = Array.from(this.data.keys());
-  }
-
-  delete(day: string, hour: number, observation: number): void {
-    this.data.get(day)[hour].observations.splice(observation, 1);
-  }
-
-  hasObservations(day: Array<ObservationGroup>): boolean {
-    return day.some(group => group.observations.length);
   }
 
   format(date: string, format: string): string {

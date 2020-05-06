4c2f592c99335e42bea7180eceb4dfb1086680aa
Jan <janewaldable@gmail.com>
2020-05-06 23:17:56 +0200

    added drag and drop infrastructure to re-order categories and delete annotations.

--- insights/src/app/visualisations/calendar-view/view-dialog/view-dialog.component.ts
+++ insights/src/app/visualisations/calendar-view/view-dialog/view-dialog.component.ts
@@ -2,6 +2,7 @@ import {Component, Inject, OnInit} from '@angular/core';
 import {MAT_DIALOG_DATA} from '@angular/material/dialog';
 import {AnnotationDetails} from '../../../shared/models';
 import {dateFormat, hoursFromDetails, moment} from '../../../shared/utils';
+import {CdkDragDrop, moveItemInArray, transferArrayItem} from '@angular/cdk/drag-drop';
 
 @Component({
   selector: 'app-view-dialog',
@@ -13,6 +14,8 @@ export class ViewDialogComponent implements OnInit {
   hours = ['01', '02', '03', '04', '05', '06', '07', '08', '09', '10', '11', '12', '13', '14', '15', '16', '17', '18', '19', '20', '21',
     '22', '23', '00'
   ];
+  delete: boolean;
+  toDelete = [];
   constructor(@Inject(MAT_DIALOG_DATA) public data: AnnotationDetails) { }
 
   ngOnInit(): void {
@@ -23,5 +26,30 @@ export class ViewDialogComponent implements OnInit {
 
   format(date: string, format: string): string {
     return moment(date, dateFormat).format(format);
+  }
+  drop(event: CdkDragDrop<string[]>, list: Array<any>) {
+    moveItemInArray(list, event.previousIndex, event.currentIndex);
+  }
+
+  dragStart(event): void {
+    this.delete = true;
+  }
+
+  dragEnd(event): void {
+    this.delete = false;
+  }
+  deleteItem(event): void {
+    console.log('delete');
+    if (event.previousContainer === event.container) {
+      moveItemInArray(event.container.data, event.previousIndex, event.currentIndex);
+    } else {
+      transferArrayItem(event.previousContainer.data,
+        event.container.data,
+        event.previousIndex,
+        event.currentIndex);
+    }
+  }
+  noReturnPredicate(): boolean {
+    return false;
   }
 }
--- insights/src/app/visualisations/visualisations.module.ts
+++ insights/src/app/visualisations/visualisations.module.ts
@@ -1,44 +1,47 @@
-import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
-import { CalendarViewComponent } from './calendar-view/calendar-view.component';
-import { VisualisationsComponent } from './visualisations.component';
+import {NgModule} from '@angular/core';
+import {CommonModule} from '@angular/common';
+import {CalendarViewComponent} from './calendar-view/calendar-view.component';
+import {VisualisationsComponent} from './visualisations.component';
 import {VisualisationsRoutingModule} from './visualisations-routing.module';
 import {SharedModule} from '../shared/shared.module';
 import {MatIconModule} from '@angular/material/icon';
 import {MatButtonModule} from '@angular/material/button';
-import { AnnotationDialogComponent } from './calendar-view/annotation-dialog/annotation-dialog.component';
-import { GesturesViewComponent } from './gestures-view/gestures-view.component';
+import {AnnotationDialogComponent} from './calendar-view/annotation-dialog/annotation-dialog.component';
+import {GesturesViewComponent} from './gestures-view/gestures-view.component';
 import {MatRippleModule} from '@angular/material/core';
 import {MatSlideToggleModule} from '@angular/material/slide-toggle';
 import {FormsModule} from '@angular/forms';
 import {MatDialogModule} from '@angular/material/dialog';
-import { FilterDialogComponent } from './calendar-view/filter-dialog/filter-dialog.component';
+import {FilterDialogComponent} from './calendar-view/filter-dialog/filter-dialog.component';
 import {A11yModule} from '@angular/cdk/a11y';
-import { ViewDialogComponent } from './calendar-view/view-dialog/view-dialog.component';
+import {ViewDialogComponent} from './calendar-view/view-dialog/view-dialog.component';
+import {DragDropModule} from '@angular/cdk/drag-drop';
 
 @NgModule({
-    declarations: [
-      CalendarViewComponent,
-      VisualisationsComponent,
-      AnnotationDialogComponent,
-      GesturesViewComponent,
-      FilterDialogComponent,
-      ViewDialogComponent
-    ],
-    exports: [
-      SharedModule
-    ],
-    imports: [
-        CommonModule,
-        VisualisationsRoutingModule,
-        SharedModule,
-        MatIconModule,
-        MatButtonModule,
-        MatRippleModule,
-        MatSlideToggleModule,
-        FormsModule,
-        MatDialogModule,
-        A11yModule
-    ]
+  declarations: [
+    CalendarViewComponent,
+    VisualisationsComponent,
+    AnnotationDialogComponent,
+    GesturesViewComponent,
+    FilterDialogComponent,
+    ViewDialogComponent
+  ],
+  exports: [
+    SharedModule
+  ],
+  imports: [
+    CommonModule,
+    VisualisationsRoutingModule,
+    SharedModule,
+    MatIconModule,
+    MatButtonModule,
+    MatRippleModule,
+    MatSlideToggleModule,
+    FormsModule,
+    MatDialogModule,
+    A11yModule,
+    DragDropModule
+  ]
 })
-export class VisualisationsModule { }
+export class VisualisationsModule {
+}

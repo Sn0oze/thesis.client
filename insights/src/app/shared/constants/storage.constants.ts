5e73ab816ab7576d56ccd6db849a8f6418909251
Jan <janewaldable@gmail.com>
2020-05-18 21:50:49 +0200

    added service to persistently communicate app settings between components.

--- insights/src/app/shared/constants/storage.constants.ts
+++ insights/src/app/shared/constants/storage.constants.ts
@@ -1,1 +1,2 @@
 export const ANNOTATIONS_KEY = 'thesis-annotations';
+export const BARS_KEY = 'thesis-bars';
---
+++ insights/src/app/shared/services/settings.service.ts
@@ -0,0 +1,17 @@
+import { Injectable } from '@angular/core';
+import {BehaviorSubject} from 'rxjs';
+import {BARS_KEY} from '../constants';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class SettingsService {
+  bars = new BehaviorSubject<boolean>(!!localStorage.getItem(BARS_KEY));
+  constructor() { }
+
+  toggle(): void {
+    const value = !this.bars.value;
+    localStorage.setItem(BARS_KEY, String(value));
+    this.bars.next(value);
+  }
+}
